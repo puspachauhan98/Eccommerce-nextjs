0baf50ed22d9ea7e54fc6e96b4225774f45efabe
puspachauhan98 <puspachauhan01@gmail.com>
2025-05-04 15:28:04 +0545

    updated proudct page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,14 @@
 import { ProductList } from "../../components/product-list";
 import { stripe } from "../../lib/stripe";
+import Stripe from "stripe";
 
+// Declare the correct type for the products
+interface ProductPageProps {
+  products: Stripe.Product[];
+}
 
 export default async function ProductsPage() {
+  // Fetch products from Stripe
   const products = await stripe.products.list({
     expand: ["data.default_price"],
   });
@@ -12,6 +18,7 @@ export default async function ProductsPage() {
       <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
         All Products
       </h1>
+      {/* Pass the products to the ProductList component */}
       <ProductList products={products.data} />
     </div>
   );
